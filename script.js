d93a1b41bbde06e1fe2031caa686ad0abe607606
Edmilton Santos <edmiltonsnts@gmail.com>
2023-03-20 10:17:15 -0300

    alterando nome img

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
-    //Metodo mais extenso//
-//if(html.classList.contains('light')) {
-// html.classList.remove('light')
-//   } else {
-//html.classList.add('light')
-//}
-//pegar a tagn img
-const img = document.querySelector("#profile img")
-//substituir a imagem 
-if (html.classList.contains("light")) {
+  //Metodo curto//
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //Metodo mais extenso//
+  //if(html.classList.contains('light')) {
+  // html.classList.remove('light')
+  //   } else {
+  //html.classList.add('light')
+  //}
+  //pegar a tagn img
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
     //Se tiver light mode, add a imagem 2
     img.setAttribute("src", "./assets/avatar2.png")
-} else {
+  } else {
     //Se tiver sem, manter o normal
     img.setAttribute("src", "./assets/avatar.png")
-}
+  }
 }
