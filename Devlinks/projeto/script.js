f682c970c9891f2a566489b201ac78575061a262
Edmilton Santos <edmiltonsnts@gmail.com>
2023-03-20 09:35:45 -0300

    trocando img

--- Devlinks/projeto/script.js
+++ Devlinks/projeto/script.js
@@ -1,16 +1,20 @@
 function toggleMode() {
     const html = document.documentElement
-
-//Metodo mais extenso//
+    html.classList.toggle('light')
+    //Metodo mais extenso//
 //if(html.classList.contains('light')) {
 // html.classList.remove('light')
 //   } else {
-//{
 //html.classList.add('light')
 //}
-//Metodo mais curto//
-html.classList.toggle('light')
+//pegar a tagn img
+const img = document.querySelector("#profile img")
+//substituir a imagem 
+if (html.classList.contains("light")) {
+    //Se tiver light mode, add a imagem 2
+    img.setAttribute("src", "./assets/avatar2.png")
+} else {
+    //Se tiver sem, manter o normal
+    img.setAttribute("src", "./assets/avatar.pnj")
 }
-
-
-
+}
